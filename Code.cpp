7db9f3bef91eaf48c988c9e137316cbae9cdecda
huynam3ntu <95837475+huynam3ntu@users.noreply.github.com>
2021-12-28 00:07:43 +0700

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -11,14 +11,14 @@ struct bigint {
     bigint() :
         sign(1) {
     }
-    bigint(const string &s) {	//Chuyen doi chuoi nhap thanh so nguyen lon
+    bigint(const string &s) {	//Nhan doi so dau vao la chuoi
         read(s);
     }
-    void operator = (const bigint &v) {
+    void operator=(const bigint &v) {
         sign = v.sign;
         a = v.a;
     }
-    bigint operator - () const {
+    bigint operator-() const {
         bigint res = *this;
         res.sign = -sign;
         return res;
@@ -27,12 +27,12 @@ struct bigint {
     bigint(long long v) {
         *this = v;
     }
-    void operator = (long long v) {
+    void operator=(long long v) {
         sign = 1;
         if (v < 0)
             sign = -1, v = -v;
         for (; v > 0; v = v / base)
-            a.push_back(v % base);
+            a.push_back(v % base); //them __ vao sau vecto a
     }
     long long longValue() const {
         long long res = 0;
@@ -51,9 +51,9 @@ struct bigint {
     friend ostream& operator << (ostream &stream, const bigint &v) {
         if (v.sign == -1)
             stream << '-';
-        stream << (v.a.empty() ? 0 : v.a.back());
+        stream << (v.a.empty() ? 0 : v.a.back()); //kiem tra vecto cua v co rong hay khong? TRUE: 0 FALSE truy cap den phan tu cuoi cung cua vector
         for (int i = (int) v.a.size() - 2; i >= 0; --i)
-            stream << setw(base_digits) << setfill('0') << v.a[i];
+            stream << setw(base_digits) << setfill('0') << v.a[i]; //setw: dieu chinh lai size, setfill la xac dinh ki tu them vao
         return stream;
     }
     
@@ -68,7 +68,7 @@ struct bigint {
         res /= v;
         return res;
     }
-	int operator % (int v) const {	//Chia du 2 so nguyen
+	int operator%(int v) const {	//Chia du 2 so nguyen
         if (v < 0)
             v = -v;
         int m = 0;
@@ -82,7 +82,7 @@ struct bigint {
             sign = -sign, v = -v;
         for (int i = 0, carry = 0; i < (int) a.size() || carry; ++i) {
             if (i == (int) a.size())
-                a.push_back(0);
+                a.push_back(0);	//Them 0 vao sau cung cua vector a
             long long cur = a[i] * (long long) v + carry;
             carry = (int) (cur / base);
             a[i] = (int) (cur % base);
@@ -99,6 +99,7 @@ struct bigint {
         }
         trim();
     }
+    
     //Ghi de, dinh nghia lai cac toan tu dung de so sanh <, >, <=, >=, ==, != giua 2 so nguyen lon
     bool operator < (const bigint &v) const {
         if (sign != v.sign)
@@ -130,10 +131,10 @@ struct bigint {
     bigint operator + (const bigint &v) const {	//Cong 2 so nguyen lon
         if (sign == v.sign) {
             bigint res = v;
- 
+            
             for (int i = 0, carry = 0; i < (int) max(a.size(), v.a.size()) || carry; ++i) {
                 if (i == (int) res.a.size())
-                    res.a.push_back(0);
+                    res.a.push_back(0);	//them 0 vao sau vector cua res
                 res.a[i] += carry + (i < (int) a.size() ? a[i] : 0);
                 carry = res.a[i] >= base;
                 if (carry)
@@ -166,23 +167,23 @@ struct bigint {
     bigint operator % (const bigint &v) const { //Mod 2 so nguyen lon
         return divmod(*this, v).second;
     }
-    bigint operator*(const bigint &v) const {	//Nhan 2 so nguyen lon
+    bigint operator * (const bigint &v) const {	//Nhan 2 so nguyen lon
         vector<int> a6 = convert_base(this->a, base_digits, 6);
         vector<int> b6 = convert_base(v.a, base_digits, 6);
         vll a(a6.begin(), a6.end());
         vll b(b6.begin(), b6.end());
         while (a.size() < b.size())
-            a.push_back(0);
+            a.push_back(0);	//Them 0 vao sau vector a
         while (b.size() < a.size())
-            b.push_back(0);
+            b.push_back(0);	//Them 0 vao sau vector b
         while (a.size() & (a.size() - 1))
-            a.push_back(0), b.push_back(0);
+            a.push_back(0), b.push_back(0);	//Them 0 vao sau 2 vector a, b
         vll c = karatsubaMultiply(a, b);
         bigint res;
         res.sign = sign * v.sign;
         for (int i = 0, carry = 0; i < (int) c.size(); i++) {
             long long cur = c[i] + carry;
-            res.a.push_back((int) (cur % 1000000));
+            res.a.push_back((int) (cur % 1000000));	//Them __ vao sau vector cua res
             carry = (int) (cur / 1000000);
         }
         res.a = convert_base(res.a, 6, base_digits);
@@ -202,18 +203,11 @@ struct bigint {
     void operator /= (const bigint &v) {
         *this = *this / v;
     }
-    //Ham tinh toan: UCLN, BCNN
-    friend bigint gcd(const bigint &a, const bigint &b) { //Tim Uoc chung lon nhat cua 2 so nguyen lon
-        return b.isZero() ? a : gcd(b, a % b);
-    }
-    friend bigint lcm(const bigint &a, const bigint &b) { //Tim Boi chung nho nhat cua 2 so nguyen lon
-        return a / gcd(a, b) * b;
-    }
     
     //Ham ho tro
     void trim() {
         while (!a.empty() && !a.back())
-            a.pop_back();
+            a.pop_back();	//Loai bo phan tu cuoi cung trong vector
         if (a.empty())
             sign = 1;
     }
@@ -230,11 +224,11 @@ struct bigint {
             int x = 0;
             for (int j = max(pos, i - base_digits + 1); j <= i; j++)
                 x = x * 10 + s[j] - '0';
-            a.push_back(x);
+            a.push_back(x);	//Them x vao sau vector a
         }
         trim();
     }
-    bigint abs() const {	//Tra ve gia tri tuyet doi cua so nguyen lon
+    bigint abs() const {	//Tra ve gia tri tuyet doi
         bigint res = *this;
         res.sign *= res.sign;
         return res;
@@ -244,7 +238,7 @@ struct bigint {
         bigint a = a1.abs() * norm;
         bigint b = b1.abs() * norm;
         bigint q, r;
-        q.a.resize(a.a.size());
+        q.a.resize(a.a.size());	//Thay doi kich thuoc vector cua q
  
         for (int i = a.a.size() - 1; i >= 0; i--) {
             r *= base;
@@ -264,7 +258,7 @@ struct bigint {
         r.trim();
         return make_pair(q, r / norm);
     }
-	static vector<int> convert_base(const vector<int> &a, int old_digits, int new_digits) {
+	static vector<int> convert_base(const vector<int> &a, int old_digits, int new_digits) { //chuyen doi co so vector
         vector<long long> p(max(old_digits, new_digits) + 1);
         p[0] = 1;
         for (int i = 1; i < (int) p.size(); i++)
@@ -276,14 +270,14 @@ struct bigint {
             cur += a[i] * p[cur_digits];
             cur_digits += old_digits;
             while (cur_digits >= new_digits) {
-                res.push_back(int(cur % p[new_digits]));
+                res.push_back(int(cur % p[new_digits]));	//Them __ vao sau vector cua res
                 cur /= p[new_digits];
                 cur_digits -= new_digits;
             }
         }
-        res.push_back((int) cur);
+        res.push_back((int) cur);	//Them __ vao sau vector cua res
         while (!res.empty() && !res.back())
-            res.pop_back();
+            res.pop_back();	//Xoa phan tu cuoi cung cua vector res
         return res;
     }
     static vll karatsubaMultiply(const vll &a, const vll &b) {	//Thuat toan nhan nhanh: giam so lan nhan giua 2 so nguyen lon
@@ -323,48 +317,49 @@ struct bigint {
         for (int i = 0; i < (int) a2b2.size(); i++)
             res[i + n] += a2b2[i];
         return res;
-    }
-    bool isZero() const {	//Kiem tra vecto rong
-        return a.empty() || (a.size() == 1 && !a[0]);
-    }  
+    } 
 };
 
 int main(){
 	bigint n1, n2;
-    cout << "\nSo thu nhat: ";
+    cout << "\n\nSo thu nhat: ";
     cin >> n1;
     cout << "\nSo thu hai: ";
     cin >> n2;
     
+    //Test toan tu so sanh
+    if (n1>n2)
+		cout << "\nso thu nhat > so thu hai\n";
+	else
+		if(n1<n2)
+			cout << "\nso thu nhat < so thu hai\n";
+		else
+			cout << "\nso thu nhat = so thu hai\n";
+			
     //Test cong 2 so
-    cout << "Tong 2 so = " << (n1 + n2) << '\n';
+    cout << n1 << " + " << n2 << " = " << (n1+n2) << '\n';
     //Test tru 2 so
-    cout << "Tru 2 so = " << (n1 - n2) << '\n';
-    
-    //Tim UCLN, BCNN
-//    n1 = 9999, n2=111;
-//    cout << "UCLN: " << gcd(n1,n2) << '\n';
-//    cout << "BCNN: " << lcm(n1,n2) << '\n';
-    
-    //Nhan 2 so - su dung ham tao bigint tu bien string
-    string s1 = "12345", s2 = "-5";
-    bigint n3(s1), n4(s2);
+    cout << n1 << " - " << n2 << " = " << (n1-n2) << '\n';
+
+	//Nhan 2 so - su dung ham tao bigint tu bien string
+    string s1 = "12345678987654321", s2 = "-1357111264246893";
+    bigint n3(s1), n4(s2);	//big int nhan doi so dau vao la string
     cout << s1 << " * " << s2 << " = " << (n3*n4) << '\n';
     cout << s1 << " / " << s2 << " = " << (n3/n4) << '\n';
-    
-    // su dung bang cac toan tu +=, -=, *=, /=, %=
-    n1 *= n2;
-    cout << s1 << " * " << s2 << " = " << n1 << '\n';
+    // su dung bang toan tu *= da dinh nghia o tren de so sanh ket qua
+    n3 *= n4;
+    cout << s1 << " * " << s2 << " = " << n3 << '\n';
     
     //demo tinh giai thua 1 so n
-//    bigint answer = 1;
-//    int N;
-//    cout << "Nhap so N = ";
-//    cin >> N;
-//    // Tinh giai thua
-//    for(int i = 2; i <= N; ++i){
-//        answer *= i;
-//    }
-//    cout << N <<"! = " << answer << '\n';
+    bigint answer = 1;
+    int N;
+    cout << "Nhap so N = ";
+    cin >> N;
+    // Tinh giai thua
+    for(int i = 2; i <= N; ++i){
+        answer *= i; //su dung toan tu *= da dinh nghia
+    }
+    cout << N <<"! = " << answer << '\n';
+    
 	return 0;
 }
